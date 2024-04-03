424e0548535118673ecc57d6ec92eb1532fc01c3
pwnkitty <165656265+pwnkitty@users.noreply.github.com>
2024-04-04 00:36:23 +0530

    Create ipkitty2.py

---
+++ ipkitty2.py
@@ -0,0 +1,85 @@
+# -*- coding: utf-8 -*-
+
+import time
+import os
+import subprocess
+
+
+
+
+try:
+    check_pip3 = subprocess.check_output('dpkg -s python3-pip', shell=True)
+    if str('install ok installed') in str(check_pip3):
+        pass
+except subprocess.CalledProcessError:
+    print('[+] pip3 not installed')
+    subprocess.check_output('sudo apt update',shell=True)
+    subprocess.check_output('sudo apt install python3-pip -y', shell=True)
+    print('[!] pip3 installed succesfully')
+
+
+
+try:
+
+    import requests
+except Exception:
+    print('[+] python3 requests is not installed')
+    os.system('pip3 install requests')
+    os.system('pip3 install requests[socks]')
+    print('[!] python3 requests is installed ')
+try:
+
+    check_tor = subprocess.check_output('which tor', shell=True)
+except subprocess.CalledProcessError:
+
+    print('[+] tor is not installed !')
+    subprocess.check_output('sudo apt update',shell=True)
+    subprocess.check_output('sudo apt install tor -y',shell=True)
+    print('[!] tor is installed succesfully ')
+
+os.system("clear")
+def ma_ip():
+    url='https://www.myexternalip.com/raw'
+    get_ip= requests.get(url,proxies=dict(http='socks5://127.0.0.1:9050',https='socks5://127.0.0.1:9050'))
+    return get_ip.text
+
+def change():
+    os.system("service tor reload")
+    print ('[+] Your Current IP Is : '+str(ma_ip()))
+
+print('''\033[1;32;40m \n
+░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░▒▓████████▓▒░▒▓████████▓▒░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░   ░▒▓█▓▒░░▒▓█▓▒░ 
+░▒▓███████▓▒░░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓███████▓▒░░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░    ░▒▓██████▓▒░  
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░     
+░▒▓█▓▒░      ░▒▓█▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░     
+░▒▓█▓▒░       ░▒▓█████████████▓▒░░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░░▒▓█▓▒░▒▓█▓▒░  ░▒▓█▓▒░      ░▒▓█▓▒░      ░▒▓█▓▒░     
+''')
+print("\033[1;40;31m Follow On Instagram @pwnkitty <3\n")
+
+os.system("service tor start")
+
+
+
+
+time.sleep(3)
+print("\033[1;32;40m change your  SOCKES to 127.0.0.1:9050 \n")
+os.system("service tor start")
+x = input("[+] interval to change Ip in Sec [type=60] >> ")
+lin = input("[+] how many time do you want to change your ip? >>")
+if int(lin) ==int(0):
+
+	while True:
+		try:
+			time.sleep(int(x))
+			change()
+		except KeyboardInterrupt:
+
+		 	print('\nipkitty is closed ')
+		 	quit()
+
+else:
+	for i in range(int(lin)):
+		    time.sleep(int(x))
+		    change()
